2360993ab07aedb9ed8cda45833b4a347645a1d4
Javier Orozco <desarrollotic1@ttncompany.com>
2024-07-06 14:28:14 -0500

    feat: setting up the index.html template and images we are going to display in the project

--- restaurant/views.py
+++ restaurant/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def index(request):
+    "This function is just for return the index web page"
+    return render(request, 'index.html', {})
