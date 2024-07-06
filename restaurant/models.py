a388bf5d85d03b4b96e750d4af71a49003cae709
Javier Orozco <desarrollotic1@ttncompany.com>
2024-07-06 15:44:46 -0500

    fix: deleting the css folder and pass their content inside the css folder inside the static folder at app level

--- restaurant/models.py
+++ restaurant/models.py
@@ -10,6 +10,9 @@ class Booking(models.Model):
     guests_number = models.IntegerField()
     booking_date = models.DateTimeField()
 
+    def __str__(self) -> str:
+        return self.name
+
 class Menu(models.Model):
     """
     This class is mapping Menu table and
@@ -18,3 +21,6 @@ class Menu(models.Model):
     title = models.CharField(max_length=255)
     price = models.DecimalField(max_digits=6, decimal_places=2, null=False)
     inventory = models.IntegerField()
+
+    def __str__(self) -> str:
+        return self.title
