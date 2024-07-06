e27b7d15740626e10cbecac342997312dfe0638c
Javier Orozco <desarrollotic1@ttncompany.com>
2024-07-06 15:28:36 -0500

    feat: creating two new models in the models.py file. One corresponding to booking table and the other to menu table

--- restaurant/models.py
+++ restaurant/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+class Booking(models.Model):
+    """
+    This class is mapping the booking table and
+    his attributes
+    """
+    name = models.CharField(max_length=255)
+    guests_number = models.IntegerField()
+    booking_date = models.DateTimeField()
+
+class Menu(models.Model):
+    """
+    This class is mapping Menu table and
+    his attributes
+    """
+    title = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=6, decimal_places=2, null=False)
+    inventory = models.IntegerField()
