08412f74632b2b297ae342ef60bf47fc73cd251a
Javier Orozco <desarrollotic1@ttncompany.com>
2024-07-17 07:33:25 -0500

    feat: adding token based authentication and user registration using djoser and another libraries

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from . import views
+from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('api/user', views.UserView.as_view(), name='user'),
     path('api/menu', views.MenuItemsView.as_view()),
     path('api/menu/<int:pk>', views.SingleMenuItemView.as_view()),
+    path('api-token-auth/', obtain_auth_token)
 ]
