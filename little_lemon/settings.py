a64a4dd7004227cac870d2f1ce28cf4fbfb5e2de
Javier Orozco <desarrollotic1@ttncompany.com>
2024-07-17 09:30:41 -0500

    feat: creating a test folder and adding test_models and test_views files inside it

--- little_lemon/settings.py
+++ little_lemon/settings.py
@@ -137,11 +137,7 @@ DJOSER = {"USER_ID_FIELD":"username"}
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework.authentication.TokenAuthentication',  # Ensure these are strings
+        'rest_framework.authentication.TokenAuthentication',
         'rest_framework.authentication.SessionAuthentication',
-    ),
-    'DEFAULT_PERMISSION_CLASSES': (
-        'rest_framework.permissions.IsAuthenticated',
-
-    ),
+    )
 }
